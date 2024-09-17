8952778dc93e2fa9425e8200965455b4bb02189f
ItzKoTiK <110309368+ItzKoTiK@users.noreply.github.com>
2024-09-17 10:08:03 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ document.addEventListener("DOMContentLoaded", function() {
     function getCurrentWeekNumber() {
         const options = { timeZone: 'Europe/Kiev' };
         const currentDate = new Date().toLocaleString('en-US', options);
-        const startOfYear = new Date(currentDate.getFullYear(), 0, 1);
-        const daysDifference = (currentDate - startOfYear) / (1000 * 60 * 60 * 24);
+        const date = new Date(currentDate);
+
+        const startOfYear = new Date(date.getFullYear(), 0, 1);
+        const daysDifference = (date - startOfYear) / (1000 * 60 * 60 * 24);
         return Math.ceil((daysDifference + startOfYear.getDay() + 1) / 7);
     }
 
@@ -12,12 +14,14 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 
     function getDayMessageAndClass() {
-        const today = new Date();
-        const dayOfWeek = today.getDay() + 1; // 0 = Sunday
+        const options = { timeZone: 'Europe/Kiev' };
+        const today = new Date().toLocaleString('en-US', options);
+        const date = new Date(today);
+        const dayOfWeek = date.getDay(); // 0 = Sunday
         const currentWeekNumber = getCurrentWeekNumber();
         let message, classForWeek;
 
-        if (dayOfWeek === 2) {
+        if (dayOfWeek === 2) { 
             message = "Сьогодні в нас";
             classForWeek = getClassForWeek(currentWeekNumber);
         } else if (dayOfWeek === 1) {
@@ -25,7 +29,7 @@ document.addEventListener("DOMContentLoaded", function() {
             classForWeek = getClassForWeek(currentWeekNumber);
         } else if (dayOfWeek > 2 && dayOfWeek <= 6 || dayOfWeek === 0) {
             message = "Наступного тижня в нас";
-            classForWeek = getClassForWeek(currentWeekNumber + 1); 
+            classForWeek = getClassForWeek(currentWeekNumber + 1);
         }
 
         return { message, classForWeek };
